1176667d2adb307cd7cfa8eef326ad31c08b999d
Onodwa2001 <onodwasiyotula7@gmail.com>
2023-03-01 20:18:17 +0200

    changed names of items

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,17 +4,17 @@ import { useState } from 'react';
 
 function Home() {
     const items = [
-        {name: "I'm awesome shirt", image: 'gorrilla.jpg', price: 230, available: true, key: 1},
-        {name: "Adidas Watch", image: 'purefit.jpg', price: 310, available: false, key: 2},
-        {name: "Guess Jeans", image: 'whey.jpg', price: 880, available: true, key: 3},
+        {name: "Nutritech Vegan Gorrilla Trainer", image: 'gorrilla.jpg', price: 230, available: true, key: 1},
+        {name: "Purefit USN Pro Enduro", image: 'purefit.jpg', price: 310, available: false, key: 2},
+        {name: "Nutritech Premium Whey Protein", image: 'whey.jpg', price: 880, available: true, key: 3},
         {name: "G-Star Raw Jeans", image: 'homebg.jpeg', price: 1200, available: false, key: 4},
-        {name: "Nike Sweatpants", image: 'nutritech-hulk-gainer-plus-free-nuke.jpg', price: 800, available: true, key: 5}
+        {name: "Nurtitech Hulk Gainer plus free Nuke", image: 'nutritech-hulk-gainer-plus-free-nuke.jpg', price: 800, available: true, key: 5}
     ]
 
     return(
         <div className="Home">
             <Navbar />
-            <MarketItems items={items} title={"Available Items"} />
+            <MarketItems items={items} title={"Supplement store"} />
         </div>
     )
 }
